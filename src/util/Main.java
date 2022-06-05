d43cc708e268971b7fc8d7ef500cf96665cdfc98
misofflo <61591151+misofflo@users.noreply.github.com>
2022-06-05 17:16:15 +0200

    improved visualizer GUI

--- src/util/Main.java
+++ src/util/Main.java
@@ -2,15 +2,8 @@ package util;
 
 import visualization.*;
 
-import javax.swing.*;
-
 public class Main {
     public static void main(String[] args) {
-        GUI window = new GUI(1000, 1000);
-        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        window.setSize(1000, 1000);
-        window.setLocationRelativeTo(null);
-        window.setResizable(false);
-        window.setVisible(true);
+        GUI window = new GUI(10, 10);
     }
 }
--- src/visualization/GUI.java
+++ src/visualization/GUI.java
@@ -2,26 +2,32 @@ package visualization;
 
 import util.*;
 import javax.swing.*;
+import java.awt.*;
+import java.util.Random;
 
 public class GUI extends JFrame {
     private GridTile[][] tiles;
 
     public GUI(int width, int height) {
         setTitle("world.World visuals.Visualizer");
-        setSize(width, height);
+        setSize(900, 900);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLayout(null);
-        setResizable(false);
+        setResizable(true);
         setVisible(true);
 
+        Random rand = new Random();
         tiles = new GridTile[width][height];
         for (int x = 0; x < width; x++) {
             for (int y = 0; y < height; y++) {
                 tiles[x][y] = new GridTile(new int[x][y], false);
-                tiles[x][y].setBounds(x * 100 + 3, y * 100 + 3, 100, 100);
+                tiles[x][y].setBounds(x * getSize().width/10, y*getSize().height/10, getSize().width/10, getSize().height/10);
+                tiles[x][y].setBackground(new Color(rand.nextInt(255), rand.nextInt(255), rand.nextInt(255)));
                 add(tiles[x][y]);
             }
         }
+
+        repaint();
     }
 }
